d88a2ba8b004c1d78f063c701136dd3bad21316f
Aly Walid <alywalid720@gmail.com>
2024-08-28 22:12:36 +0300

    V1 restored

--- Car.java
+++ Car.java
@@ -3,8 +3,6 @@ public class Car {
     public static void main(String[] args) {
         Car c1 = Car.initCar();
         Car c2 = Car.initCar();
-        System.out.println("Version 2");  
-        System.out.println("Version 3");          
     }
 
     private Car(){
