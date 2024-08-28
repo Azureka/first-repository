8961a6ed3be0c9c14126fa46bc209c66c006ee90
Aly Walid <alywalid720@gmail.com>
2024-08-28 21:05:16 +0300

    V1

---
+++ Car.java
@@ -0,0 +1,18 @@
+public class Car {
+    private static Car car;
+    public static void main(String[] args) {
+        Car c1 = Car.initCar();
+        Car c2 = Car.initCar();
+    }
+
+    private Car(){
+        System.out.println("Dah first object.");
+    }
+
+    public static Car initCar() {
+        if(car == null)
+            car = new Car();
+        
+        return car;
+    }
+}
